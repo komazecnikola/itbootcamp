6c9c44bf2c8508af800f207c7a1f12e9fe55c7b3
komazecnikola <nikola.koma@gmail.com>
2023-12-29 19:24:18 +0100

    eventovi zadatak 7 dodat

--- 20_EVENTS/script.js
+++ 20_EVENTS/script.js
@@ -103,3 +103,46 @@ btnIspis.addEventListener("dblclick", () => {
     }
   });
 });
+
+// Zadatak 7
+
+// Pod A
+let kvadratInput = document.getElementById("kvadratInput");
+let kvadratDugme = document.getElementById("kvadratButton");
+let kvadratParagraf = document.getElementById("kvadratRezultat");
+kvadratDugme.addEventListener("click", (e) => {
+  let broj = kvadratInput.value;
+  kvadratParagraf.innerText = `Kvadrat ovog broja je ${broj * broj}`;
+  e.preventDefault(); // ovo može da se stavi da bismo sprečili da se stranica reloaduje nakon što kliknemo na dugme
+});
+
+// Pod B
+let polaInput = document.getElementById("polaInput");
+let polaDugme = document.getElementById("polaButton");
+let polaParagraf = document.getElementById("polaRezultat");
+polaDugme.addEventListener("click", () => {
+  let broj = polaInput.value;
+  polaParagraf.innerText = `Polovina ovog broja je ${broj / 2}`;
+});
+
+// Pod C
+let precnikInput = document.getElementById("precnikInput");
+let precnikDugme = document.getElementById("precnikButton");
+let precnikParagraf = document.getElementById("precnikRezultat");
+let radioObim = document.getElementById("obimKruga");
+let radioPovrsina = document.getElementById("povrsinaKruga");
+precnikDugme.addEventListener("click", () => {
+  let r = precnikInput.value;
+  if (radioObim.checked) {
+    precnikParagraf.innerText = `Obim kruga za ovaj poluprečnik je ${
+      2 * r * 3.14
+    }`;
+  }
+  if (radioPovrsina.checked) {
+    precnikParagraf.innerText = `Površina kruga za ovaj poluprečnik je ${
+      r * r * 3.14
+    }`;
+  }
+
+  // Može i ovako: Math.pow(r, 2) * Math.PI
+});
